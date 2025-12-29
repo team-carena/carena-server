6862a303487269ba9f83b0a2b7b97fa2f0400706
kymin <park48664866@gmail.com>
2025-12-29 16:26:48 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/org/sopt/carena/CarenaApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.carena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sopt/carena/CarenaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.carena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
