f0f700a652f666d59af06973804bb0af3b56de67
kymin <park48664866@gmail.com>
2026-01-02 17:35:16 +0900

    feat: 공통 응답 및 반환 코드 인터페이스 작성

---
+++ src/main/java/org/sopt/carena/global/api/code/ErrorResultCode.java
@@ -0,0 +1,7 @@
+package org.sopt.carena.global.api.code;
+
+/**
+ * 실패 응답 마커 인터페이스
+ */
+public interface ErrorResultCode extends ResultCode{
+}
---
+++ src/main/java/org/sopt/carena/global/api/code/SuccessResultCode.java
@@ -0,0 +1,7 @@
+package org.sopt.carena.global.api.code;
+
+/**
+ * 성공 응답 마커 인터페이스
+ */
+public interface SuccessResultCode extends ResultCode {
+}
