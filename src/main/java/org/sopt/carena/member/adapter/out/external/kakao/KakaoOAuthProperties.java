5b2b16dd778e2d303a7f1e3b44864bfbd8851d1c
byunhm02 <byunhm02@gmail.com>
2026-01-07 22:35:14 +0900

    feat: KakaoOAuthProperties 추가

--- src/main/java/org/sopt/carena/member/adapter/out/external/kakao/KakaoOAuthProperties.java
+++ src/main/java/org/sopt/carena/member/adapter/out/external/kakao/KakaoOAuthProperties.java
@@ -1,4 +1,15 @@
 package org.sopt.carena.member.adapter.out.external.kakao;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "oauth.kakao")
+@Getter
+@Setter
 public class KakaoOAuthProperties {
+    private String clientId;
+    private String redirectUri;
 }
