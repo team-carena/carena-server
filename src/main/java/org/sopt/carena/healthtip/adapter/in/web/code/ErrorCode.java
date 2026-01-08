1b0835b13e2ea747067fad56f98aa2d20b3c0a45
kymin <park48664866@gmail.com>
2026-01-08 13:24:46 +0900

    feat: 응답 상태코드 및 예외 클래스 구현

---
+++ src/main/java/org/sopt/carena/healthtip/adapter/in/web/code/ErrorCode.java
@@ -0,0 +1,16 @@
+package org.sopt.carena.healthtip.adapter.in.web.code;
+
+import org.sopt.carena.global.api.code.ErrorResultCode;
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode implements ErrorResultCode {
+	HEALTH_TIP_NOTFOUND(HttpStatus.NOT_FOUND, "해당 ID의 건강팁을 찾을 수 없습니다.");
+
+	private final HttpStatus status;
+	private final String message;
+}
---
+++ src/main/java/org/sopt/carena/healthtip/adapter/in/web/code/SucessCode.java
@@ -0,0 +1,19 @@
+package org.sopt.carena.healthtip.adapter.in.web.code;
+
+import org.sopt.carena.global.api.code.SuccessResultCode;
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SucessCode implements SuccessResultCode {
+	HEALTH_TIP_CREATED(HttpStatus.CREATED,"건강팁이 생성되었습니다."),
+	HEALTH_TIP_FOUNDED(HttpStatus.OK, "건강팁 조회가 완료되었습니다."),
+	HEALTH_TIP_UPDATED(HttpStatus.OK, "건강팁이 수정되었습니다."),
+	HEALTH_TIP_DELETED(HttpStatus.OK, "건강팁이 삭제되었습니다.");
+
+	private final HttpStatus status;
+	private final String message;
+}
