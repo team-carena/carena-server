46afb1c9309ca8a8a7deef1d5c97c556de58508b
kymin <park48664866@gmail.com>
2026-01-08 15:40:15 +0900

    refactor: 연관관계 인덱스 설정

--- src/main/java/org/sopt/carena/healthtip/adapter/out/persistence/entity/HealthTipHashtagEntity.java
+++ src/main/java/org/sopt/carena/healthtip/adapter/out/persistence/entity/HealthTipHashtagEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -15,7 +16,11 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "health_tip_hashtag")
+@Table(name = "health_tip_hashtag",
+		indexes = {
+				@Index(name = "idx_health_tip_hashtag_health_tip_id", columnList = "health_tip_id"),
+				@Index(name = "idx_health_tip_hashtag_hashtag_id", columnList = "hashtag_id")
+		})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HealthTipHashtagEntity {
 	@Id
