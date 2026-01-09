935975484772999d26169f2571d306f6080037a6
kymin <park48664866@gmail.com>
2026-01-10 01:30:56 +0900

    refactor: 요청값에 null체크 추가

--- src/main/java/org/sopt/carena/healthtip/adapter/in/request/CreateHealthTipRequest.java
+++ src/main/java/org/sopt/carena/healthtip/adapter/in/request/CreateHealthTipRequest.java
@@ -3,6 +3,7 @@ package org.sopt.carena.healthtip.adapter.in.request;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateHealthTipRequest(
 		@NotBlank
@@ -17,6 +18,7 @@ public record CreateHealthTipRequest(
 		@NotBlank
 		String reference,
 
+		@NotNull
 		List<@NotBlank String> hashtags
 ) {
 }
