cd51b75e1263a12864f0ab5f722f77549c164ad4
kymin <park48664866@gmail.com>
2026-01-09 16:39:09 +0900

    refactor: 미사용 클래스 제거

--- src/main/java/org/sopt/carena/healthtip/adapter/out/persistence/repository/HealthTipSearchRepository.java
+++
@@ -1,9 +0,0 @@
-// package org.sopt.carena.healthtip.adapter.out.persistence.repository;
-//
-// import org.sopt.carena.healthtip.adapter.out.persistence.entity.HealthTipEntity;
-// import org.springframework.data.domain.Pageable;
-// import org.springframework.data.domain.Slice;
-//
-// public interface HealthTipSearchRepository {
-// 	Slice<HealthTipEntity> getHealthTipListWithHashTags(Pageable pageable);
-// }
