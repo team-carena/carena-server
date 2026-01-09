9a8173d27f744190b7ef855e485d408177274b5c
kymin <park48664866@gmail.com>
2026-01-09 14:13:16 +0900

    feat: validation에 의한 에외 처리 추가

--- src/main/java/org/sopt/carena/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/carena/global/exception/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.sopt.carena.global.exception.code.ErrorCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -30,13 +31,19 @@ public class GlobalExceptionHandler extends BaseExceptionHandler {
 		return buildErrorResponse(ErrorCode.INVALID_REQUEST_MESSAGE);
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		return buildErrorResponse(ErrorCode.INVALID_REQUEST_MESSAGE);
+	}
+
 	@ExceptionHandler(NoHandlerFoundException.class)
 	protected ResponseEntity<ApiResponse> handleNoHandlerFoundException(NoHandlerFoundException e) {
 		return buildErrorResponse(ErrorCode.INVALID_ENDPOINT);
 	}
 
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-	protected ResponseEntity<ApiResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+	protected ResponseEntity<ApiResponse> handleHttpRequestMethodNotSupportedException(
+			HttpRequestMethodNotSupportedException e) {
 		return buildErrorResponse(ErrorCode.INVALID_REQUEST_METHOD);
 	}
 
