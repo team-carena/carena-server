135a88c24f5c0f3c904f216507ce7e72981d22ab
byunhm02 <byunhm02@gmail.com>
2026-01-11 11:03:15 +0900

    chore: code 제거

--- src/main/java/org/sopt/carena/member/domain/AuthType.java
+++ src/main/java/org/sopt/carena/member/domain/AuthType.java
@@ -1,20 +1,5 @@
 package org.sopt.carena.member.domain;
 
-import org.sopt.carena.member.appliacation.exception.oauth.UnsupportedOAuthProviderException;
-
-import java.util.Arrays;
-
 public enum AuthType {
     KAKAO;
-
-    public static AuthType from(String provider) {
-        if (provider == null || provider.isBlank()) {
-            throw new UnsupportedOAuthProviderException();
-        }
-
-        return Arrays.stream(values())
-                .filter(type -> type.name().equalsIgnoreCase(provider))
-                .findFirst()
-                .orElseThrow(UnsupportedOAuthProviderException::new);
-    }
 }
--- src/main/java/org/sopt/carena/member/domain/Gender.java
+++ src/main/java/org/sopt/carena/member/domain/Gender.java
@@ -1,11 +1,6 @@
 package org.sopt.carena.member.domain;
 
 public enum Gender {
-    MALE("MALE"),
-    FEMALE("FEMALE");
-
-    private final String code;
-    Gender(String code) {
-        this.code = code;
-    }
+    MALE,
+    FEMALE;
 }
