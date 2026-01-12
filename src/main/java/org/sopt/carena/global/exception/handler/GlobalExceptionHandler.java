6f670c4607a2bf2c9f0a6f5df7f5517b4107bed5
kymin <park48664866@gmail.com>
2026-01-12 23:28:05 +0900

    feat: 요청 파라미터 검증 예외 처리 추가

--- src/main/java/org/sopt/carena/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/carena/global/exception/handler/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -33,6 +34,11 @@ public class GlobalExceptionHandler extends BaseExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	protected ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		return buildErrorResponse(ErrorCode.INVALID_REQUEST_MESSAGE);
+	}
+
+	@ExceptionHandler(HandlerMethodValidationException.class)
+	protected ResponseEntity<ApiResponse> handleMethodValidationException(HandlerMethodValidationException e) {
 		return buildErrorResponse(ErrorCode.INVALID_REQUEST_MESSAGE);
 	}
 
