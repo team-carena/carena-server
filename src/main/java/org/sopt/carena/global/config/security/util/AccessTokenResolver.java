6ff40371801b3775ea55207d594f0ed914393719
byunhm02 <byunhm02@gmail.com>
2026-01-15 04:43:41 +0900

    refactor: 얘외처리 분리

--- src/main/java/org/sopt/carena/global/config/security/util/AccessTokenResolver.java
+++ src/main/java/org/sopt/carena/global/config/security/util/AccessTokenResolver.java
@@ -2,18 +2,20 @@ package org.sopt.carena.global.config.security.util;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.sopt.carena.member.exception.jwt.EmptyTokenException;
-import org.springframework.stereotype.Component;
+import org.sopt.carena.member.exception.jwt.MalformedTokenException;
 
 // 헤더에서 엑세스 토큰을 추출하는 유ㅇ틸
 public class AccessTokenResolver {
 
     public static String resolve(HttpServletRequest request) {
         String header = request.getHeader("Authorization");
-
-        if (header == null || !header.startsWith("Bearer ")) {
+        if (header == null) {
             throw new EmptyTokenException();
         }
 
+        if (!header.startsWith("Bearer ")) {
+            throw new MalformedTokenException();
+        }
         return header.substring(7);
     }
 }
