679ed65af5c53916078988b970c0cb26e2c3ef98
byunhm02 <byunhm02@gmail.com>
2026-01-14 16:35:32 +0900

    refactor: 불필요한 파일 삭제

--- src/main/java/org/sopt/carena/member/appliacation/port/in/GetMemberInfoUseCase.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.carena.member.appliacation.port.in;
-
-import org.sopt.carena.member.application.dto.view.MemberInfoView;
-
-public interface GetMemberInfoUseCase {
-    MemberInfoView getMemberInfo(Long memberId, boolean withScore);
-}
