a77cef7e67eccdff4251a118220124466b0536c4
byunhm02 <byunhm02@gmail.com>
2026-01-18 20:37:49 +0900

    refactor: content추가

--- src/main/java/org/sopt/carena/diet/adapter/in/web/dto/request/CreateAdminDietRequest.java
+++ src/main/java/org/sopt/carena/diet/adapter/in/web/dto/request/CreateAdminDietRequest.java
@@ -1,12 +1,15 @@
 package org.sopt.carena.diet.adapter.in.web.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 import java.util.Map;
 
 public record CreateAdminDietRequest (
-        String title,
-        String reference,
-        String referenceUrl,
+        @NotBlank String title,
+        @NotBlank String content,
+        @NotBlank String reference,
+        @NotBlank String referenceUrl,
         List<AdminDietChunkRequest> chunks,
         Map<String, List<String>> recommendedFoods,
         List<String> cautionaryFoods
