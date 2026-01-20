5b02ec14948c60222b613568995c8f9619383a3c
byunhm02 <byunhm02@gmail.com>
2026-01-20 16:21:53 +0900

    chore: 네이밍 변경

--- src/main/java/org/sopt/carena/member/application/service/MemberHealthScoreService.java
+++ src/main/java/org/sopt/carena/member/application/service/MemberHealthScoreService.java
@@ -4,13 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.sopt.carena.healthreport.domain.HealthReport;
 import org.sopt.carena.healthreport.domain.score.HealthScore;
 import org.sopt.carena.healthreport.domain.score.caculator.HealthScoreCalculator;
+import org.sopt.carena.member.application.port.in.HealthScoreUseCase;
 import org.sopt.carena.member.application.port.out.MemberPersistencePort;
 import org.sopt.carena.member.domain.Member;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberHealthScoreService {
+public class MemberHealthScoreService implements HealthScoreUseCase {
 
     private final HealthScoreCalculator healthScoreCalculator;
     private final MemberPersistencePort memberPersistencePort;
