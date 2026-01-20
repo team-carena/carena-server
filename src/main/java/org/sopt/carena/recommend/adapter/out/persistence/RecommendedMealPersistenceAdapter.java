d48c2144299934fad91097d4a5149179ada24733
kymin <park48664866@gmail.com>
2026-01-20 22:40:45 +0900

    fix: 트랜잭션 누락 수정

--- src/main/java/org/sopt/carena/recommend/adapter/out/persistence/RecommendedMealPersistenceAdapter.java
+++ src/main/java/org/sopt/carena/recommend/adapter/out/persistence/RecommendedMealPersistenceAdapter.java
@@ -6,7 +6,6 @@ import org.sopt.carena.healthreport.application.port.out.SaveRecommendedMealPort
 import org.sopt.carena.recommend.adapter.out.persistence.entity.RecommendedMealEntity;
 import org.sopt.carena.recommend.adapter.out.persistence.mapper.RecommendedMealMapper;
 import org.sopt.carena.recommend.adapter.out.persistence.repository.RecommendedMealRepository;
-import org.sopt.carena.recommend.application.port.in.GetLatestRecommendedMealUseCase;
 import org.sopt.carena.recommend.application.port.out.GetLatestRecommendedMealPort;
 import org.sopt.carena.recommend.domain.RecommendedMeal;
 import org.sopt.carena.healthreport.adapter.out.persistence.entity.HealthReportEntity;
@@ -14,16 +13,19 @@ import org.sopt.carena.healthreport.adapter.out.persistence.repository.HealthRep
 import org.sopt.carena.member.adapter.out.persistence.entity.MemberEntity;
 import org.sopt.carena.member.adapter.out.persistence.repository.MemberJpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RecommendedMealPersistenceAdapter implements SaveRecommendedMealPort, GetLatestRecommendedMealPort {
 	private final RecommendedMealRepository recommendedMealRepository;
 	private final MemberJpaRepository memberJpaRepository;
 	private final HealthReportRepository healthReportRepository;
 
+	@Transactional
 	public RecommendedMeal saveRecommendedMeal(final RecommendedMeal recommendedMeal) {
 		MemberEntity memberProxy = memberJpaRepository.getReferenceById(recommendedMeal.getMemberId());
 		HealthReportEntity healthReportProxy = healthReportRepository.getReferenceById(recommendedMeal.getHealthReportId());
