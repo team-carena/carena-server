f547343593114bd6abcccfc4777e91b3598cf4fb
kymin <park48664866@gmail.com>
2026-01-20 16:28:46 +0900

    refactor: 임베딩 문장 컨버터 패키지 변경

---
+++ src/main/java/org/sopt/carena/healthreport/application/converter/HealthReportEmbeddingConverter.java
@@ -0,0 +1,19 @@
+package org.sopt.carena.healthreport.application.converter;
+
+import java.util.stream.Collectors;
+
+import org.sopt.carena.healthreport.domain.HealthReport;
+import org.sopt.carena.healthreport.domain.status.HealthStatusCarrier;
+import org.sopt.carena.healthreport.domain.status.RiskLevel;
+
+public class HealthReportEmbeddingConverter {
+	public static String toEmbeddingText(HealthReport healthReport) {
+		return healthReport.getStatusCarriers().stream()
+				.filter(carrier -> carrier.getRiskLevel() != RiskLevel.NONE)
+				.map(HealthStatusCarrier::getDescription)
+				.collect(Collectors.collectingAndThen(
+						Collectors.joining(", "),
+						s -> s.isEmpty() ? "건강 검진 결과 데이터가 존재하지 않음" : s
+				));
+	}
+}
