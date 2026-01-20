fca4c604635b1945353db0c8bf43f1d1e8a7cda3
byunhm02 <byunhm02@gmail.com>
2026-01-20 13:48:26 +0900

    hotfix: cors origin 임시 추가

--- src/main/java/org/sopt/carena/global/config/security/SecurityConfig.java
+++ src/main/java/org/sopt/carena/global/config/security/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(List.of("https://carena-client.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("https://carena-client.vercel.app","http://localhost:5173"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setExposedHeaders(List.of("Authorization"));
