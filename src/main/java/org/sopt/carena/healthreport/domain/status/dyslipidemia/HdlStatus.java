509d29bb84b18636a234c4728c9a84c6cebc1b37
kymin <park48664866@gmail.com>
2026-01-22 03:36:12 +0900

    hotfix: 임베딩을 위한 라벨링 수정

--- src/main/java/org/sopt/carena/healthreport/domain/status/dyslipidemia/HdlStatus.java
+++ src/main/java/org/sopt/carena/healthreport/domain/status/dyslipidemia/HdlStatus.java
@@ -11,8 +11,8 @@ import lombok.RequiredArgsConstructor;
 public enum HdlStatus implements HealthStatusCarrier {
 	NONE("없음", RiskLevel.NONE),
 	NORMAL("HDL 수치 정상, 고밀도 콜레스테롤 수치 정상", RiskLevel.NORMAL),
-	HDL_BORDERLINE("HDL 수치 다소 낮음, 고밀도 콜레스테롤 수치 다소 낮음, 심혈관 보호 기능 감소 가능, 낮은 HDL 콜레스테롤 의심", RiskLevel.BORDERLINE),
-	HDL_SUSPICIOUS("HDL 수치 낮음, 고밀도 콜레스테롤 수치 낮음, 심혈관 보호 기능 저하, 낮은 HDL 콜레스테롤 의심", RiskLevel.SUSPICIOUS);
+	HDL_BORDERLINE("HDL 수치 다소 낮음, 고밀도 콜레스테롤 수치 다소 낮음, 심혈관 보호 기능 감소 가능", RiskLevel.BORDERLINE),
+	HDL_SUSPICIOUS("HDL 수치 낮음, 고밀도 콜레스테롤 수치 낮음, 심혈관 보호 기능 저하", RiskLevel.SUSPICIOUS);
 
 	private final String description;
 	private final RiskLevel riskLevel;
