46bad1e5edd164889dbae867180c1d8c5b51dfd0
kymin <park48664866@gmail.com>
2026-02-23 22:48:49 +0900

    refactor: 애플리케이션에 재시도 로직 적용

--- src/main/java/org/sopt/carena/CarenaApplication.java
+++ src/main/java/org/sopt/carena/CarenaApplication.java
@@ -3,7 +3,9 @@ package org.sopt.carena;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class CarenaApplication {
