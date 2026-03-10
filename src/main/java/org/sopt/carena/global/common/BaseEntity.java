ad6965d25ba1f1ef7e41123633ed3fceffac5b2d
kymin <park48664866@gmail.com>
2026-03-11 00:02:18 +0900

    fix: 검진결과 수정 후 생성일자 누락 버그 수정

--- src/main/java/org/sopt/carena/global/common/BaseEntity.java
+++ src/main/java/org/sopt/carena/global/common/BaseEntity.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -14,5 +15,6 @@ import lombok.Getter;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 	@CreatedDate
+	@Column(updatable = false)
 	private LocalDateTime createdAt;
 }
